25bd3e64cf24a5d34cfaa703101fe5d657084eae
PreessionS <theroymaster@gmail.com>
2024-10-10 14:00:33 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ from diffusers import FluxPipeline
 
 pipe = FluxPipeline.from_pretrained("black-forest-labs/FLUX.1-dev", torch_dtype=torch.bfloat16)
 pipe.enable_model_cpu_offload() #save some VRAM by offloading the model to CPU. Remove this if you have enough GPU power
+pipe = DiffusionPipeline.from_pretrained(
+    "black-forest-labs/FLUX.1-dev",
+    torch_dtype=torch.bfloat16,
+    use_auth_token="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"
+)
 
 prompt = "A cat holding a sign that says hello world"
 image = pipe(
